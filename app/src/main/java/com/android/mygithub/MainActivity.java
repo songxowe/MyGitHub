0ca9fdf1e9af5aaa1ae3d2654f2578de20acc5bd
songxowe <songxowe@hotmail.com>
2015-11-11 13:54:10 +0800

    by songfsad

--- app/src/main/java/com/android/mygithub/MainActivity.java
+++ app/src/main/java/com/android/mygithub/MainActivity.java
@@ -3,6 +3,9 @@ package com.android.mygithub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * by song
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
